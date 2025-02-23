141dc9748161c0fb87bf12e46104096e99571b05
Vinod Patidar <vinodpatidar0808@gmail.com>
2025-02-23 17:49:31 +0530

    basic ui flow

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <input className="input" type="text" />
+      <button className="button">Send</button>
+      {/* TODO: progress bar */}
+    </div>
+  )
+}
+
+export default Sidebar
